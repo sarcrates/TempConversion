60ca63cec90636071d7348564a8dd351265f22f9
Hana Sakamoto <sarcrates_dev@icloud.com>
2013-12-26 10:55:30 -0700

    Initial Commit

---
+++ TempConversion/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TempConversion
+//
+//  Created by sarcrates on 12/26/13.
+//  Copyright © 2013 sarcrates. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
